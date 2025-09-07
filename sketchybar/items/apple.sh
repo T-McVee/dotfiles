a0d4f80e0bb781cb21fee9b1a7adab29fe311e6f
Tim McVinish <iam@tmcvee.com>
2025-09-06 21:58:06 -0700

    Adds demo popup menu

---
+++ sketchybar/items/apple.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source "$CONFIG_DIR/colors.sh" # Loads all defined colors
+
+# Apple logo demo
+sketchybar --add item apple.logo left \
+           --set apple.logo icon=􀣺  \
+                 icon.font="SF Pro:Black:16.0" \
+                 label.drawing=off \
+                 click_script="sketchybar -m --set \$NAME popup.drawing=toggle" \
+                 popup.background.border_width=2 \
+                 popup.background.corner_radius=5 \
+                 popup.background.color=$SECONDARY_COLOR \
+                 popup.background.border_color=$BAR_COLOR \
+           --add item apple.preferences popup.apple.logo \
+           --set apple.preferences icon=􀺽 \
+                 label="Preferences" \
+                 label.color=$BLACK \
+                 icon.color=$BLACK \
+                 click_script="open -a 'System Preferences'; \
+                               sketchybar -m --set apple.logo popup.drawing=off" \
+           --add item apple.lock popup.apple.logo \
+           --set apple.lock icon=􀒳 \
+                            label="Lock Screen" \
+                            label.color=$BLACK \
+                            icon.color=$BLACK \
+                            click_script="pmset displaysleepnow;                           
+                                          sketchybar -m --set apple.logo popup.drawing=off"
