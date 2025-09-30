ee3a06f13345ad346e8b144658cbb694dc3fccea
Tim McVinish <iam@tmcvee.com>
2025-09-30 22:05:06 +1000

    Highlight battery when below 50%

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -8,22 +8,23 @@ if [ "$PERCENTAGE" = "" ]; then
 fi
 
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON="􀛨"
+  9[0-9]|100) ICON="􀛨" COLOR=0xffFFFFFF
   ;;
-  [6-8][0-9]) ICON="􀺸"
+  [6-8][0-9]) ICON="􀺸" COLOR=0xffFFFFFF
   ;;
-  [3-5][0-9]) ICON="􀺶"
+  [3-5][0-9]) ICON="􀺶" COLOR=0xffBC7E62
   ;;
-  [1-2][0-9]) ICON="􀛩"
+  [1-2][0-9]) ICON="􀛩" COLOR=0xffBC7E62
   ;;
-  *) ICON="􀛪"
+  *) ICON="􀛪" COLOR=0xffBC7E62
 esac
 
 if [[ "$CHARGING" != "" ]]; then
-  ICON="􀢋"
+  ICON="􀢋" COLOR=0xffFFFFFF
+  # COLOR=0xffBC7E62
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
 # sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
-sketchybar --set "$NAME" icon="$ICON"
+sketchybar --set "$NAME" icon="$ICON" icon.color=$COLOR
